c7fa9069e15be00bd300078c06eb0d87765d7445
izaaaliine <izaline.18@gmail.com>
2023-11-14 16:22:48 +0100

    ajout

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
-// // // Exercice 1
+// // // Exercice 1 ----------------------
+
 // // // Créer 4 variables : nom, prenom, age, taille
 // // // affichez dans la console ces 4 variables (vous pouvez séparer les variables par une virgule dans le console.log())
 // // // Ajoutez 2 à l'age
@@ -15,7 +16,7 @@
 
 // console.log(name, prenom, taille, age);
 
-// // // Exercice 2 :
+// // // Exercice 2 ----------------------
 
 // // // Reprenez le premier exercice et à l'aide de la concaténation, affectez le tout à une nouvelle variable.
 // // // Affichez cette nouvelle variable dans la console.
@@ -24,14 +25,14 @@
 
 // console.log(total);
 
-// // // Exercice 3 :
+// // // Exercice 3 ----------------------
 
 // // // Reprenez le résultat de l'exercice 2, et à la place d'affecter le prenom à la variable prénom, utilisez la fonction prompt pour assigner ce qu'un utilisateur écrit à la variable prenom
 
 // prenom = prompt(`Quel est votre prenom ?`);
 // console.log(prenom);
 
-// // Exercice 4 :
+// // Exercice 4 ----------------------
 
 // // Reprenez le résultat de l'exercice 2, et cette fois ci, demandé à l'utilisateur d'entrer un age. Si l'age est supérieur ou égale à 18, affichez dans la console "je suis majeur", sinon affichez dans la console "je ne suis pas majeur"
 
@@ -43,7 +44,7 @@
 //   console.log(`Je ne suis pas majeur`);
 // }
 
-// Exercice 5 :
+// Exercice 5 ----------------------
 
 // Mathéo a une moyenne de 15,4 , Nathan de 9,2 , Léo de 16,1 et Maxime de 12,6. Calculez la moyenne de ces 4 personnes
 
@@ -55,14 +56,14 @@
 
 // console.log(moyenne);
 
-// Exercice 6 :
+// Exercice 6 ----------------------
 
 // A l'aide de math.floor et math.random, affichez dans la console un nombre aléatoire compris entre 1 et 100. (cet exercice vous demandera d'aller lire la doc)
 
 // let joker = Math.floor(Math.random() * 100) + 1;
 // console.log(joker);
 
-// Exercice 7 :
+// Exercice 7 ----------------------
 
 // A faire uniquement en JS
 // Créer un élement h1 avec comme contenu Hello World, une classe name "monH1".
@@ -80,7 +81,7 @@
 // document.body.insertBefore(texte, titre);
 // // ou h1.parentNode.insertBefore(texte, titre);
 
-// Exercice 8 :
+// Exercice 8 ----------------------
 
 // L'utilisateur doit entrer un nombre. Si ce nombre est positif, alors on doit récupérer une valeur aléatoire compris entre 1 et ce nombre
 // l'afficher dans la console
@@ -91,7 +92,7 @@
 //   console.log(resultat);
 // }
 
-// Exercice 9
+// Exercice 9 ----------------------
 
 // A l'aide de ce tableau :
 // Afficher dans la console Tracer
@@ -101,30 +102,31 @@
 // Ajouter une chaine de caractère "nop", pour simuler l'ajout d'une erreur
 // Faire une recherche pour savoir comment supprimer un élément dans un tableau et supprimer "nop"
 
-let championOverwatch = [
-  "Ashe",
-  "Echo",
-  "Tracer",
-  "Reaper",
-  "Phara",
-  "McCree",
-  "Soldier:76",
-  "Genji",
-  "Sombra",
-  "Doomfist",
-  "Widowmaker",
-  "Torbjorn",
-  "Hanzo",
-  "Bastion",
-  "Junkrat",
-  "Mei",
-  "Symmetra",
-];
+// let championOverwatch = [
+//   "Ashe",
+//   "Echo",
+//   "Tracer",
+//   "Reaper",
+//   "Phara",
+//   "McCree",
+//   "Soldier:76",
+//   "Genji",
+//   "Sombra",
+//   "Doomfist",
+//   "Widowmaker",
+//   "Torbjorn",
+//   "Hanzo",
+//   "Bastion",
+//   "Junkrat",
+//   "Mei",
+//   "Symmetra",
+// ];
 
 // console.log(championOverwatch[2]);
 // console.log(championOverwatch[15]);
 
 // console.log((championOverwatch[5] = "Cole Cassidy"));
+// console.tab => pour avoir les élements sous forme d'un tableau
 
 // championOverwatch.push("Ramattra", "nop");
 // console.log(championOverwatch);
@@ -132,7 +134,7 @@ let championOverwatch = [
 // championOverwatch.pop();
 // console.log(championOverwatch);
 
-// Exercice 10
+// Exercice 10 ----------------------
 
 // Convertir le tableau en chaine de caractère
 // Afficher une copie du tableau qui va de Genji à Hanzo (faite une recherche sur slice)
@@ -148,17 +150,14 @@ let championOverwatch = [
 // console.log(championOverwatch);
 // console.log(championOverwatch.length);
 
-// Exercice 11
+// Exercice 11 ----------------------
+
 // A l'aide de la boucle foreach, concaténer tous les héros dans une variable pour faire apparaitre dans la console "Les héros d'Overwatch sont : [Tableau des Heros d'Overwatch]
 
-let phrase = "Les héros d'Overwatch sont : ";
+// let phrase = "Les héros d'Overwatch sont : ";
 
 championOverwatch.forEach((e) => {
   phrase += e + ", ";
 });
-
-console.log(phrase);
-
-phrase = phrase.slice(0, -2);
-phrase += "]";
+phrase = phrase.substring(0, phrase.length - 2) + ".";
 console.log(phrase);
